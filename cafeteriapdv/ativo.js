aaa3391a03e1012991d7b14ae17ac9653f7efad3
Vitoriarsilva <155200054+Vitoriarsilva@users.noreply.github.com>
2025-07-08 14:07:48 -0300

    Delete cafeteriapdv/ativo.js

--- cafeteriapdv/ativo.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll('.botao-header');
-const currentUrl = window.location.href;
-
-links.forEach(link => {
-  if (currentUrl.includes(link.getAttribute('href'))) {
-    link.classList.add('ativa');
-  }
-});
